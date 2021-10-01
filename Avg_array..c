d56ca75247831eee5c1f7a34b2ca3554e74dbc49
B-Anupam <71441129+B-Anupam@users.noreply.github.com>
2021-10-01 16:19:38 +0530

    Update Avg_array..c

--- Avg_array..c
+++ Avg_array..c
@@ -2,21 +2,24 @@
 #include<stdio.h>
 int main()
 {
-	int a[10],i,j;
+	int i,j,size;
 	float avg;
+	printf("Enter the size of arraay");
+	scanf("%d\n",&size);
+	int a[size];
 	printf("Enter the value of the array:\n");
-	for(i=0;i<10;i++)
+	for(i=0;i<size;i++)
 	{
-		scanf("%d\n",a[i]);
+		scanf("%d\n",&a[i]);
 	}
 	printf("find the avrage:\n");
-	for(j=0;j<10;j++)
+	for(j=0;j<size;j++)
 	{
 		avg= avg+a[i];
 	}
 	printf("Avrage Marks: %f",avg);
 	
-getch();
+
 return 0;
 }	
 
